09a52495f848bc473de725fc9b2281d744981d39
sakebook <sakebook@gmail.com>
2013-12-14 17:59:55 +0900

    modify dialog cancel

--- src/com/sakebook/android/uploadhelper/Const.java
+++ src/com/sakebook/android/uploadhelper/Const.java
@@ -5,5 +5,6 @@ public class Const {
 	public static String TAG = "UploadHelper";
 	public static String CLIENT_ID = "";
 	public static String CLIENT_SECRET_ID = "";
+	public static String UPLOAD_URL = "https://api.imgur.com/3/image";
 	
 }
