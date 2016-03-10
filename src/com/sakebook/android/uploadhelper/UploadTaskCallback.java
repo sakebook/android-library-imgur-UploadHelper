ccec6096e94e585149bf8a35904de5080699bb94
Shinya Sakemoto <sakebook@gmail.com>
2016-03-11 01:11:30 +0900

    Remove no use resource

--- src/com/sakebook/android/uploadhelper/UploadTaskCallback.java
+++
@@ -1,9 +0,0 @@
-package com.sakebook.android.uploadhelper;
-
-public interface UploadTaskCallback {
-	
-	public void success(String url);
-	public void cancel(String message);
-	public void fail(String message);
-
-}
